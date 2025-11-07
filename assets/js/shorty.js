93ae736fcc9b3f075d5f566690e4cf4c81000b14
Max Jonas Werner <max@coppersoft.com>
2025-11-07 16:36:33 +0100

    Better feedback message
    Some reverse proxies don't return a status text so treat it as optional
    in the feedback overlay.

--- assets/js/shorty.js
+++ assets/js/shorty.js
@@ -18,7 +18,11 @@ function shorten(url, key) {
     req.addEventListener("load", function () {
         var res = document.querySelector("#result");
         if (this.status !== 200) {
-            feedback(this.statusText + ": " + this.responseText);
+            let txt = this.responseText;
+            if (this.statusText.length > 0) {
+                txt = this.statusText + ": " + txt
+            }
+            feedback(txt);
             return;
         }
         res.value = this.responseText;
